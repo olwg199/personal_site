f3f24f54272d734a124e9ce3b5aa6674184a3fe2
Oleh Yastrebov <olwg199@gmail.com>
2021-06-30 18:38:32 -0400

    Extract scripts into separate file scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("a.nav-link").click(function (event) {
+      event.preventDefault();
+      $("html, body").animate(
+        { scrollTop: $($(this).attr("href")).offset().top },
+        750
+      );
+    });
+  });
